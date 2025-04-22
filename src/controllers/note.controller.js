53c457ccbff706260fdfa53564b69a8d9b0e7c39
Yeagerist-Nandini <nandinij324@gmail.com>
2025-04-22 22:48:03 +0530

    Notes controller completed

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,1 +1,91 @@
- 
+import { asyncHandler } from "../utils/async-handler.js";
+import ProjectNote from "../models/note.models.js"
+import { ApiError } from "../utils/api-error";
+import {ApiResponse} from "../utils/api-response.js"
+
+const getNotes = asyncHandler(async (req, res) => {
+    const {projectId} = req.params;
+
+    const notes = await ProjectNote.find({project: projectId});
+
+    if(!notes){
+        throw new ApiError(404, "notes not found");
+    }
+
+    return res
+           .status(200)
+           .json(new ApiResponse(200, notes, "Notes fetched successfully!"));
+});
+
+const getNoteById = asyncHandler(async (req, res) => {
+    const {noteId} = req.params;
+
+    const note = await ProjectNote.findById(noteId);
+
+    if(!note){
+        throw new ApiError(404, "note not found");
+    }
+
+    return res
+           .status(200)
+           .json(new ApiResponse(200, note, "Note fetched successfully!"));
+});
+
+const createNote = asyncHandler(async (req, res) => {
+    const {projectId} = req.params;
+    /////TODO : validation
+    const {content} = req.body;
+
+    const note = await ProjectNote.create({
+        project: projectId,
+        createdBy: req.user._id,
+        content
+    })
+
+    if(!note){
+        throw new ApiError(500, "note creation failed");
+    }
+
+    return res
+           .status(200)
+           .json(new ApiResponse(200, note, "Note created successfully!"));
+});
+
+const updateNote = asyncHandler(async (req, res) => {
+    const {noteId} = req.params;
+    const {content} = req.body;
+
+    const note = await ProjectNote.findByIdAndUpdate(
+        noteId,
+        {
+            $set:{
+                content
+            },
+        },
+        { new: true }
+    )
+
+    if(!note){
+        throw new ApiError(500, "note updation failed");
+    }
+
+    return res
+           .status(200)
+           .json(new ApiResponse(200, note, "Note updated successfully!"));
+});
+
+const deleteNote = asyncHandler(async (req, res) => {
+    const {noteId} = req.params;
+
+    const note = await ProjectNote.findByIdAndDelete(noteId);
+
+    if(!note){
+        throw new ApiError(500, "note deletion failed");
+    }
+
+    return res
+           .status(200)
+           .json(new ApiResponse(200, note, "Note deleted successfully!"));
+});
+
+export { createNote, deleteNote, getNoteById, getNotes, updateNote };
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,6 +1,24 @@
 import { Router } from "express";
+import {
+    createNote, 
+    deleteNote, 
+    getNoteById, 
+    getNotes, 
+    updateNote
+} from '../controllers/note.controller.js'
+import isLoggedIn from '../middlewares/auth.middleware.js'
 
-const router = Router()
+const router = Router();
+
+router.get('/projects/:projectId/notes', isLoggedIn, getNotes);
+
+router.get('/projects/:projectId/notes/:noteId', isLoggedIn, getNoteById);
+
+router.post('/projects/:projectId/notes', isLoggedIn, createNote);
+
+router.patch('/projects/:projectId/notes/:noteId', isLoggedIn, updateNote);
+
+router.delete('/projects/:projectId/notes/:noteId', isLoggedIn, deleteNote);
 
 
 export default router
